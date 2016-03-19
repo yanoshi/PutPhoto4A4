582ba4d609778e205b6942c51251c19a3562f1f1
yanoshi <googreimu@gmail.com>
2016-03-19 16:35:41 +0900

    はじめのいっぽ

---
+++ PutPhoto4A4/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Livet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PutPhoto4A4.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        #region コンストラクタ
+        public MainWindowViewModel()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new DependencyObject()))
+            {
+                //VSデザイナ向けの処理
+                Title = "これはテストです";
+
+            }
+        }
+        #endregion
+
+        #region プロパティ
+
+        #region Title
+        private string _Title;
+        /// <summary>
+        /// 印刷物上部に表示するタイトル
+        /// </summary>
+        public string Title
+        {
+            get { return _Title; }
+            set
+            {
+                if (_Title != value)
+                {
+                    _Title = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+        #endregion
+
+        #endregion
+    }
+}
