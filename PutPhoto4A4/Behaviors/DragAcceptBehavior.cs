2e0c95d5e32e4820ff18091f7789d019174f7343
yanoshi <googreimu@gmail.com>
2016-03-20 15:08:55 +0900

    D&D用のビヘイビアをコピペ。 http://b.starwing.net/?p=131 より。

---
+++ PutPhoto4A4/Behaviors/DragAcceptBehavior.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows;
+using System.Windows.Interactivity;
+using PutPhoto4A4.Models;
+
+namespace PutPhoto4A4.Behaviors
+{
+    public sealed class DragAcceptBehavior : Behavior<FrameworkElement>
+    {
+        public DragAcceptDescription Description
+        {
+            get { return (DragAcceptDescription)GetValue(DescriptionProperty); }
+            set { SetValue(DescriptionProperty, value); }
+        }
+
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register("Description", typeof(DragAcceptDescription),
+            typeof(DragAcceptBehavior), new PropertyMetadata(null));
+
+        protected override void OnAttached()
+        {
+            this.AssociatedObject.PreviewDragOver += AssociatedObject_DragOver;
+            this.AssociatedObject.PreviewDrop += AssociatedObject_Drop;
+            base.OnAttached();
+        }
+
+        protected override void OnDetaching()
+        {
+            this.AssociatedObject.PreviewDragOver -= AssociatedObject_DragOver;
+            this.AssociatedObject.PreviewDrop -= AssociatedObject_Drop;
+            base.OnDetaching();
+        }
+
+        void AssociatedObject_DragOver(object sender, DragEventArgs e)
+        {
+            var desc = Description;
+            if (desc == null)
+            {
+                e.Effects = DragDropEffects.None;
+                e.Handled = true;
+                return;
+            }
+            desc.OnDragOver(e);
+            e.Handled = true;
+        }
+
+        void AssociatedObject_Drop(object sender, DragEventArgs e)
+        {
+            var desc = Description;
+            if (desc == null)
+            {
+                e.Effects = DragDropEffects.None;
+                e.Handled = true;
+                return;
+            }
+            desc.OnDrop(e);
+            e.Handled = true;
+        }
+    }
+}
---
+++ PutPhoto4A4/Behaviors/DragStartBehavior.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace PutPhoto4A4.Behaviors
+{
+    /// <summary>
+    /// D&Dを開始するビヘイビア
+    /// http://b.starwing.net/?p=131 から引用
+    /// </summary>
+    public class DragStartBehavior : Behavior<FrameworkElement>
+    {
+        private Point _origin;
+        private bool _isButtonDown;
+
+        public DragDropEffects AllowedEffects
+        {
+            get { return (DragDropEffects)GetValue(AllowedEffectsProperty); }
+            set { SetValue(AllowedEffectsProperty, value); }
+        }
+
+        public static readonly DependencyProperty AllowedEffectsProperty =
+            DependencyProperty.Register("AllowedEffects", typeof(DragDropEffects),
+                    typeof(DragStartBehavior), new UIPropertyMetadata(DragDropEffects.All));
+
+        public object DragDropData
+        {
+            get { return GetValue(DragDropDataProperty); }
+            set { SetValue(DragDropDataProperty, value); }
+        }
+
+        public static readonly DependencyProperty DragDropDataProperty =
+            DependencyProperty.Register("DragDropData", typeof(object),
+                    typeof(DragStartBehavior), new PropertyMetadata(null));
+
+        protected override void OnAttached()
+        {
+            this.AssociatedObject.PreviewMouseDown += AssociatedObject_PreviewMouseDown;
+            this.AssociatedObject.PreviewMouseMove += AssociatedObject_PreviewMouseMove;
+            this.AssociatedObject.PreviewMouseUp += AssociatedObject_PreviewMouseUp;
+        }
+
+        protected override void OnDetaching()
+        {
+            this.AssociatedObject.PreviewMouseDown -= AssociatedObject_PreviewMouseDown;
+            this.AssociatedObject.PreviewMouseMove -= AssociatedObject_PreviewMouseMove;
+            this.AssociatedObject.PreviewMouseUp -= AssociatedObject_PreviewMouseUp;
+        }
+
+        void AssociatedObject_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            _origin = e.GetPosition(this.AssociatedObject);
+            _isButtonDown = true;
+        }
+
+        void AssociatedObject_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed || !_isButtonDown)
+            {
+                return;
+            }
+            var point = e.GetPosition(this.AssociatedObject);
+            if (CheckDistance(point, _origin))
+            {
+                DragDrop.DoDragDrop(this.AssociatedObject, this.DragDropData, this.AllowedEffects);
+                _isButtonDown = false;
+                e.Handled = true;
+            }
+        }
+
+        void AssociatedObject_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            _isButtonDown = false;
+        }
+
+        private bool CheckDistance(Point x, Point y)
+        {
+            return Math.Abs(x.X - y.X) >= SystemParameters.MinimumHorizontalDragDistance ||
+                Math.Abs(x.Y - y.Y) >= SystemParameters.MinimumVerticalDragDistance;
+        }
+    }
+}
